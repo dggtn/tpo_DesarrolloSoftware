e1698211455fcd5cdff8008633255d9c7feaed75
Daniela <dggtn97@gmail.com>
2025-06-22 20:56:36 -0300

    muestro por pantalla cada seccion login,signup

--- src/vista/Login.java
+++ src/vista/Login.java
@@ -1,16 +1,37 @@
 package vista;
 
+import controlador.RegistroController;
 import controlador.TecladoController;
 
 public class Login extends Pantalla {
+    private final IniciarSesionController inicioSesion;
+public Login( TecladoController teclado,IniciarSesionController inicioSesion) {
+    super(teclado);
+    this.inicioSesion = inicioSesion;
 
-    public Login(TecladoController teclado) {
-        super(teclado);
-    }
-
-    @Override
-    public String mostrar() {
-        System.out.println("ME FALTO HACER EL LOGIN");
-        return "Inicio";
-    }
 }
+
+public void iniciarSesion() {
+    System.out.println("Ingresa tu correo electrónico:");
+    String email = teclado.leerLinea();
+    Login.setEmail(email);
+    System.out.println("Ingresa tu contrasena:");
+    String contrasena = teclado.leerLinea();
+    Login.setContrasena(contrasena);
+    Login.finalizarRegistro();
+}
+
+
+@Override
+public String mostrar() {
+    System.out.println("Ingresa tu correo electrónico:");
+    String email = teclado.leerLinea();
+    Login.setNombre(email);
+    System.out.println("Ingresa tu contrasena:");
+    String contrasena = teclado.leerLinea();
+    Login.setContrasena(contrasena);
+    Login.finalizarRegistro();
+    return "Fin";
+}
+}
+
