15199023eda0e56cf441d70e3d344c82a6787712
Daniela <dggtn97@gmail.com>
2025-06-25 10:49:21 -0300

    creo controller y pantalla para inscribirse a partido

---
+++ src/controlador/InscripcionAPartidoController.java
@@ -0,0 +1,7 @@
+package controlador;
+
+public class InscripcionAPartidoController {
+    public void inscribirseAPartidos(){
+
+    }
+}
--- src/vista/PantallaInscribirseAPartido.java
+++ src/vista/PantallaInscribirseAPartido.java
@@ -1,4 +1,11 @@
 package vista;
 
+import controlador.InscripcionAPartidoController;
+
 public class PantallaInscribirseAPartido {
+    private final InscripcionAPartidoController inscripcion;
+
+    public PantallaInscribirseAPartido(InscripcionAPartidoController inscripcion) {
+        this.inscripcion = inscripcion;
+    }
 }
